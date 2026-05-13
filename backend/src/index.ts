d12469e432fd96b8ab323ee0f45fb45166a6d36b
iqbal <ifachry@rocketmail.com>
2026-05-13 14:41:05 +0700

    add backfill for createdStream Event

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,16 @@
-import { startIndexer } from "./services/streamIndexer";
+import { startIndexer }
+  from "./services/streamIndexer";
+
+import { backfill }
+  from "./services/backfill";
+
 import "./api/server";
 
 async function main() {
+  // historical sync
+  await backfill();
+
+  // realtime indexing
   await startIndexer();
 }
 
