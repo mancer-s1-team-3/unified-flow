1cb8a75be53cea021474b25f1b0e321800ca99c8
iqbal <ifachry@rocketmail.com>
2026-06-21 12:58:02 +0700

    fix admin

--- frontend/src/lib/solana/admin.ts
+++ frontend/src/lib/solana/admin.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import * as anchor from "@coral-xyz/anchor";
-import idl from "../../../../backend/src/idl/unified_flow.json";
+import idl from "@/lib/idl/unified_flow.json";
 import { Buffer } from "buffer";
 import { createWalletTransactionSigner, transactionToBase64 } from "@solana/client";
 import {
